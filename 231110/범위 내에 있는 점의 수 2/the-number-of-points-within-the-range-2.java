a6e485e9b8c33040a3c9b7b9267a223b147309f7
Lee SangJin <leesj8676@gmail.com>
2023-11-10 13:43:03 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231110/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] split;
+		split = br.readLine().split(" ");
+		int N = Integer.parseInt(split[0]);
+		int Q = Integer.parseInt(split[1]);
+
+		int arr[] = new int[1_000_001]; // 수평선 (있으면 1)
+		int prefixSum[] = new int[1_000_001]; // 부분합
+
+		split = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			int p = Integer.parseInt(split[i]);
+			arr[p] = 1;
+		}
+
+		// 부분합 구하기
+		for (int i = 1; i <= 1_000_000; i++) {
+			prefixSum[i] = prefixSum[i - 1] + arr[i];
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < Q; i++) {
+			split = br.readLine().split(" ");
+			int L = Integer.parseInt(split[0]);
+			int R = Integer.parseInt(split[1]);
+			int val = prefixSum[R] - prefixSum[L - 1];
+			sb.append(val + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+}

be72b5e5fdd1713d85bbb15ac8472f053979b508
Lee SangJin <leesj8676@gmail.com>
2023-11-10 13:45:14 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 652ms, Memory: 45MB, Status: Passed - CodeTree

--- 231110/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 231110/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -9,25 +9,25 @@ public class Main {
 		int N = Integer.parseInt(split[0]);
 		int Q = Integer.parseInt(split[1]);
 
-		int arr[] = new int[1_000_001]; // 수평선 (있으면 1)
-		int prefixSum[] = new int[1_000_001]; // 부분합
+		int arr[] = new int[1_000_002]; // 수평선 (있으면 1)
+		int prefixSum[] = new int[1_000_002]; // 부분합
 
 		split = br.readLine().split(" ");
 		for (int i = 0; i < N; i++) {
-			int p = Integer.parseInt(split[i]);
+			int p = Integer.parseInt(split[i]) + 1;
 			arr[p] = 1;
 		}
 
 		// 부분합 구하기
-		for (int i = 1; i <= 1_000_000; i++) {
+		for (int i = 1; i <= 1_000_001; i++) {
 			prefixSum[i] = prefixSum[i - 1] + arr[i];
 		}
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < Q; i++) {
 			split = br.readLine().split(" ");
-			int L = Integer.parseInt(split[0]);
-			int R = Integer.parseInt(split[1]);
+			int L = Integer.parseInt(split[0]) + 1;
+			int R = Integer.parseInt(split[1]) + 1;
 			int val = prefixSum[R] - prefixSum[L - 1];
 			sb.append(val + "\n");
 		}

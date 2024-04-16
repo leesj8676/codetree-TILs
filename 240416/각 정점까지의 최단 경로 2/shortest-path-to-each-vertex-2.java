7a449199a6a375cd8eb8ff63702a63115b3e3c52
Lee SangJin <leesj8676@gmail.com>
2024-04-16 10:32:10 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 604ms, Memory: 22MB, Status: Passed - CodeTree

--- 240416/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.java
+++ 240416/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.java
@@ -44,7 +44,7 @@ public class Main {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (dist[i][j] == (int) 1e9)
-					System.out.print(-1);
+					System.out.print(-1+" ");
 				else
 					System.out.print(dist[i][j] + " ");
 			}
